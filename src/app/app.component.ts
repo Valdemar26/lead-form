fd613ba99ed550f28f455e8035efd67e666d0fed
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-11-15 18:20:02 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lead-form';
+export class AppComponent implements OnInit {
+  leadForm: FormGroup;
+  emails: FormArray;
+  phones: FormArray;
+  customs: FormArray;
+  value: string;
+
+  foods: any = [
+    {value: 'steak-0'},
+    {value: 'pizza-1'},
+    {value: 'tacos-2'}
+  ];
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.leadForm = this.formBuilder.group({
+      name: '',
+      surname: '',
+      address: '',
+      emails: this.formBuilder.array([ this.formBuilder.control('') ]),
+      phones: this.formBuilder.array([ this.formBuilder.control('') ]),
+      status: '',
+      buyer: '',
+      source: '',
+      customs: this.formBuilder.array([ this.formBuilder.control('') ])
+    });
+  }
+
+  addEmailItem(): void {
+    this.emails = this.leadForm.get('emails') as FormArray;
+    this.emails.push(this.formBuilder.control(''));
+  }
+
+  addPhoneItem(): void {
+    this.phones = this.leadForm.get('phones') as FormArray;
+    this.phones.push(this.formBuilder.control(''));
+  }
+
+  addCustomItem(): void {
+    this.customs = this.leadForm.get('custom') as FormArray;
+    this.customs.push(this.formBuilder.control(''));
+  }
+
+  submitForm() {
+    console.log(this.leadForm.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
